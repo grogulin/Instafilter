c9eb1516f1756d9e60cf755f555ba2829a7082c7
Yaroslav Grogul <time7bass@gmail.com>
2023-02-27 13:58:50 +0100

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Ярослав Грогуль on 27.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
